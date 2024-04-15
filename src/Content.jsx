1014361eea2b0272abd41f1c52204cc14c56d2c9
Jed Staley <JNStaley1@gmail.com>
2024-04-15 17:53:33 -0400

    Added image upload functionality

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,37 +1,15 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import UploadImage from './UploadImage';
+
 
 function Content(props) {
 
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-
-    async function getBooks() {
-      const config = {
-        method: 'get',
-        baseURL: import.meta.env.VITE_SERVER_URL,
-        url: '/books'
-      }
-
-      const booksResponse = await axios(config);
-
-      setBooks(booksResponse.data);
-    }
-
-    getBooks();
-
-  }, []);
-
   return (
-    <>
-      <h1>Books We Love</h1>
-      <ul>
-        {books.map(book => (
-          <li key={book._id}>{book.title}</li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <h1>File Upload Example</h1>
+      <UploadImage />
+    </div>
   );
  }
 
