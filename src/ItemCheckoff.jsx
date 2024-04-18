c8cf42b544c5453ef42e9059c3771d9cdb71b7ac
qilinxie02 <qxie@gicw.org>
2024-04-18 14:31:41 -0700

    set up for socialshare

--- src/ItemCheckoff.jsx
+++ src/ItemCheckoff.jsx
@@ -1,44 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import './css/itemCheckoff.css'; // no existing file, but if Adam wants to add CSS for styling
+import SocialShare from './SocialShare';
 
-
-const ItemCheckoff = ({ selectedFile, setCheckoffItems }) => {
+const ItemCheckoff = ({ selectedFile, setCheckoffItems, scavengerHuntId }) => {
   const [response, setResponse] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // Fetch response from AI API or backend upon component mount
-    async function fetchResponse() {
-      try {
-        const formData = new FormData();
-        formData.append('file', selectedFile);
+    if (selectedFile) {
+      setIsLoading(true);
+      const fetchResponse = async () => {
+        try {
+          const formData = new FormData();
+          formData.append('file', selectedFile);
 
-
-        const response = await axios.post(`${import.meta.env.VITE_SERVER_URL}/api/processImage`, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
+          const response = await axios.post(`${import.meta.env.VITE_SERVER_URL}/api/processImage`, formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+          });
+          setResponse(response.data);
+          setIsLoading(false);
+          if (response.data.checkedItem) {
+            setCheckoffItems(prevItems => [...prevItems, response.data.checkedItem]);
           }
-        });
-        setResponse(response.data);
-        // Update the checkoffItems state in the parent component
-        if (response.data.checkedItem) {
-          setCheckoffItems(prevItems => [...prevItems, response.data.checkedItem]);
+        } catch (error) {
+          console.error('Error fetching response:', error);
+          setIsLoading(false);
         }
-      } catch (error) {
-        console.error('Error fetching response:', error);
-      }
+      };
+      fetchResponse();
     }
-    fetchResponse();
   }, [selectedFile, setCheckoffItems]);
-
 
   return (
     <div className="checkoff-container">
-      {response ? (
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : response ? (
         <>
           <p>Photo has been approved and the item has been checked off the list.</p>
           <p>Checked off item: {response.checkedItem}</p>
+          <SocialShare scavengerHuntId={scavengerHuntId} />
         </>
       ) : (
         <p>Processing image...</p>
@@ -46,6 +47,5 @@ const ItemCheckoff = ({ selectedFile, setCheckoffItems }) => {
     </div>
   );
 };
-
 
 export default ItemCheckoff;
